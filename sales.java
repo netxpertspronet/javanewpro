938c43b5ff5d9a24c63f3dd4f6cb993ebb9c6174
Ubuntu <azureuser@VMA.4cqkvjtuk4xurawcb41nhlvozd.bx.internal.cloudapp.net>
2024-09-22 12:09:58 +0000

    New Code has been Added with Sales Module

--- sales.java
+++ sales.java
@@ -3,3 +3,8 @@ public class Main {
     System.out.println("Hello World");
   }
 }
+int myNum = 5;               // Integer (whole number)
+float myFloatNum = 5.99f;    // Floating point number
+char myLetter = 'D';         // Character
+boolean myBool = true;       // Boolean
+String myText = "Hello";     // String
